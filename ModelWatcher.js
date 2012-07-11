04789bf9f1c524e0a46cab5cf99914c0996bf1a1
Mamonth <andrew.tereshko@gmail.com>
2012-07-11 13:49:15 +0400

    model & modelWatcher early implementation

---
+++ ModelWatcher.js
@@ -0,0 +1,28 @@
+/**
+ * Model Watcher
+ *
+ * @author Andrew Tereshko <andrew.tereshko@gmail.com>
+ * @version 0.3.0
+ */
+
+define("app/ModelWatcher", [ /* "app/Model" */ ], function () {
+
+    $.Class.extend("app.ModelWatcher",
+        {
+            _storage: {},
+            register: function( model ){
+
+            },
+            isset: function( class, id ){
+
+            },
+            get: function( class, id ){
+
+            }
+        },
+        {
+
+        }
+    );
+
+});
